9e0bbf80cf6216ccd88694e7ae1de377c400902e
SuperNacho <vanbellum@gmail.com>
2018-02-07 16:35:06 +0300

    Created parser for first run of app, for fill db with countries from csv file in resources

--- app/src/main/java/fw/supernacho/ru/foxweather/CsvParser.java
+++ app/src/main/java/fw/supernacho/ru/foxweather/CsvParser.java
@@ -1,13 +1,18 @@
 package fw.supernacho.ru.foxweather;
 
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import fw.supernacho.ru.foxweather.data.DataBaseHelper;
 
@@ -17,26 +22,32 @@ import fw.supernacho.ru.foxweather.data.DataBaseHelper;
 
 public class CsvParser {
     private DataBaseHelper dbHelper;
-    private SQLiteDatabase dataBase;
     private BufferedReader bufferedReader;
-    
-    CsvParser(DataBaseHelper dbHelper){
+    private SQLiteDatabase dataBase;
+    private Context context;
+
+    public CsvParser(DataBaseHelper dbHelper, SQLiteDatabase dataBase, Context context){
         this.dbHelper = dbHelper;
+        this.dataBase = dataBase;
+        this.context = context;
     }
 
-    public void parseCSV(File file){
+    public void parseCSV(int resurcesID){
         try {
-            bufferedReader = new BufferedReader(new FileReader(file));
+            Resources resources = context.getResources();
+            bufferedReader = new BufferedReader(new InputStreamReader(resources.openRawResource(R.raw.country_codes)));
             String line =null;
             while ((line = bufferedReader.readLine()) != null){
                 String[] result = line.split(",");
                 String title = result[0];
+                Log.d("////", title);
                 String tag = result[1];
-                dataBase = dbHelper.getWritableDatabase();
+                Log.d("////", tag);
                 ContentValues values = new ContentValues();
                 values.put(DataBaseHelper.COLUMN_TITLE, title);
                 values.put(DataBaseHelper.COLUMN_TAG, tag);
-                dataBase.insert(DataBaseHelper.TABLE_COUNTRIES, null, values);
+                long ins = dataBase.insert(DataBaseHelper.TABLE_COUNTRIES, null, values);
+                Log.d("////", String.valueOf(ins));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -44,7 +55,7 @@ public class CsvParser {
             e.printStackTrace();
         } finally {
             try {
-                bufferedReader.close();
+                if ( bufferedReader != null) bufferedReader.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
